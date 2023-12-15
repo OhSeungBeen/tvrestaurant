529ed1efe3093896862e1447f061be3b30ac71f3
SeungBeenOh <seungbeenoh@gmail.com>
2023-12-15 17:17:47 +0900

    chore: modify pretendard font file name

--- apps/client/src/lib/fonts/local.ts
+++ apps/client/src/lib/fonts/local.ts
@@ -1,5 +1,5 @@
 import localFont from 'next/font/local';
 
 export const pretendard = localFont({
-  src: '../../../public/fonts/PretendardVariable.woff2',
+  src: '../../../public/fonts/Pretendard-Variable.woff2',
 });
